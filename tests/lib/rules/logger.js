e0c4bd524a1b88771a4522404699d1272fad2813
Paul Vollmer <paul.vollmer@protonmail.com>
2018-04-13 08:43:21 +0200

    logger ast check node parent exist

--- tests/lib/rules/logger.js
+++ tests/lib/rules/logger.js
@@ -2,13 +2,16 @@
 
 const rule = require('../../../lib/rules/logger')
 const RuleTester = require('eslint').RuleTester;
-
 const ruleTester = new RuleTester();
+
 ruleTester.run('example', rule, {
+
   valid: [
     'logger.info("hello world")',
     'logger.error("hello world")',
     'logger.warn("hello world")',
+    'lib.logger("ignore this one")',
+    'lib.foo.logger("ignore this one")'
   ],
 
   invalid: [
@@ -23,4 +26,5 @@ ruleTester.run('example', rule, {
       errors: [ { messageId: 'invalidLoggerMode' } ]
     }
   ]
+
 });
