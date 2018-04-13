d44f4a5481374acb780fd3da033e4362a6b6420b
Paul Vollmer <paul.vollmer@protonmail.com>
2018-04-13 08:26:04 +0200

    index added config

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 'use strict';
 
+// console.log('loopback wcl plugin')
+// console.log('-------------------');
+
 module.exports = {
+  configs: {
+    recommended: {
+      plugins: ['loopback-wcl'],
+      rules: {
+        'loopback-wcl/logger': 'warn',
+      }
+    }
+  },
   rules: {
     logger: require('./lib/rules/logger')
   }
